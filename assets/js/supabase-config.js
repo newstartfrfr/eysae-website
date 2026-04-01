c8a71c7a44adf910f8515e6cd8cd8273bfea2c93
newstartfrfr <fix.info.ponudba@proton.me>
2026-04-01 22:17:13 +0200

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,8 +1,8 @@
 export const supabaseConfig = {
-  url: "https://dnzysgfiqcyhlguticcw.supabase.co",
-  anonKey: "sb_publishable_HW5twbWGZFLP9fhL2aWjUw_gFgs_S0O",
+  url: "",
+  anonKey: "",
   postImageBucket: "post-images",
-  adminEmails: ["krizo19@gmail.com"]
+  adminEmails: ["dan.grmusa@gmail.com"]
 };
 
 export function supabaseIsConfigured(config = supabaseConfig) {
