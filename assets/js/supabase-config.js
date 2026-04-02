b5696b3f84a611d69ea095db18005d076bd37100
newstartfrfr <fix.info.ponudba@proton.me>
2026-04-02 13:09:32 +0200

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,6 +1,6 @@
 export const supabaseConfig = {
-  url: "",
-  anonKey: "",
+  url: "https://dnzysgfiqcyhlguticcw.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuenlzZ2ZpcWN5aGxndXRpY2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNzA3ODAsImV4cCI6MjA5MDY0Njc4MH0.HA_6sdN0fuaRMvzA361Al08pljKKtpU_NIYvFG2y08A",
   postImageBucket: "post-images",
   adminEmails: ["dan.grmusa@gmail.com"]
 };
